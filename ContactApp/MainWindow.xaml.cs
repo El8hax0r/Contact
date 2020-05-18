e9f3f653f52db8a3518bfe4edba7f1c30c5f25b1
Matthew Mitchell <matthewprestonmitchell@gmail.com>
2020-05-17 18:48:07 -0700

    Sort functionality added

--- ContactApp/MainWindow.xaml.cs
+++ ContactApp/MainWindow.xaml.cs
@@ -1,4 +1,13 @@
 ﻿using System.Windows;
+using ContactApp.Models;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Data;
 
 namespace ContactApp
 {
@@ -10,7 +19,30 @@ namespace ContactApp
         public MainWindow()
         {
             InitializeComponent();
+
+            LoadContacts();
         }
+        private void LoadContacts()
+        {
+            var contacts = App.ContactRepository.GetAll();
+
+            uxContactList.ItemsSource = contacts
+                .Select(t => ContactModel.ToModel(t))
+                .ToList();
+
+            // OR
+            //var uiContactModelList = new List<ContactModel>();
+            //foreach (var repositoryContactModel in contacts)
+            //{
+            //    This is the .Select(t => ... )
+            //    var uiContactModel = ContactModel.ToModel(repositoryContactModel);
+            //
+            //    uiContactModelList.Add(uiContactModel);
+            //}
+
+            //uxContactList.ItemsSource = uiContactModelList;
+        }
+
 
         private void uxFileNew_Click(object sender, RoutedEventArgs e)
         {
@@ -26,7 +58,42 @@ namespace ContactApp
 
                 // OR
                 //App.ContactRepository.Add(window.Contact.ToRepositoryModel());
+
+                LoadContacts();
             }
+        }
+        private void uxFileChange_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void uxFileDelete_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
+        private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            var column = (sender as GridViewColumnHeader);
+            
+            string sortBy = column.Tag.ToString();
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                uxContactList.Items.SortDescriptions.Clear();
+            }
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column &&
+                listViewSortAdorner.Direction == newDir)
+                newDir = ListSortDirection.Descending;
+
+            listViewSortCol = column;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add
+                (listViewSortAdorner);
+            uxContactList.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+
         }
     }
 }
